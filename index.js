18055cf3ad0fed2af93dcce2218613e8c1abb6b8
Imran Hossain <imran@yibd.org>
2023-03-23 23:50:54 +0600

    First Assigmnet

---
+++ index.js
@@ -0,0 +1,15 @@
+<script>
+    function multiplicationTable() {
+
+for (i = 1; i =< 10; i++) {
+        let product = i;
+    for (j = 1; j =< 10; j++);
+    {
+        let product = j;
+
+    document.write('${procudt}x{procudt}=${i * j} <br');
+    }
+    document.write('<br>');
+}
+}
+</script>
